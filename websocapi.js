082d3f2e280feb5c18f010a39a456a720f4b6d5e
Nathan <nabebaye@uci.edu>
2017-08-06 18:30:39 -0700

    Minimal refactoring: remove debug code.

--- websocapi.js
+++ websocapi.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const classes = require("./classes.js");
 const request = require("request");
 const cheerio = require("cheerio");
@@ -7,12 +9,12 @@ function postToWebSoc({
                           instructorName = '', courseTitle = '', classType = 'ALL', units = '', days = '',
                           startTime = '', endTime = '', maxCap = '', fullCourses = 'ANY', cancelledCourses = 'EXCLUDE',
                           building = '', room = ''
-                      }, callback) {
+                      },  callback) {
 
-    if (term === undefined) {
-        throw "Define the term"
+    if (term === undefined || term === '') {
+        throw new Error("Define the term");
     } else if (department === "ALL" && breadth === "ANY" && courseCodes === '' && instructorName === '') {
-        throw "Define at least one of department, breadth, courseCodes, or instructorName";
+        throw new Error("Define at least one of department, breadth, courseCodes, or instructorName");
     }
 
     let postData = {
@@ -41,15 +43,13 @@ function postToWebSoc({
             Room: room,
         },
     };
-    console.time('parseProcess');
+
     request.post(postData, (err, res, body) => {
-        if (!err && res.statusCode == 200) {
+        if (!err && res.statusCode === 200) {
             callback(parseForClasses(body));
-            console.timeEnd('parseProcess');
         } else {
             console.err(err);
         }
-
     });
 }
 
@@ -79,10 +79,10 @@ function parseForClasses(htmlBody) {
                 lastSchool = getLast(schools);
                 lastDept = getLast(lastSchool.departments);
                 lastDept.addComment(row.text());
-            } else if (row.children("td").hasClass("CourseTitle")) {
+            } else if (row.is("tr[valign='top'][bgcolor='#fff0ff']")) {
                 lastSchool = getLast(schools);
                 lastDept = getLast(lastSchool.departments);
-                let courseName = row.children(".CourseTitle").text();
+                let courseName = row.children().text();
                 courseName = courseName.replace(/\s+/g, ' ').replace('(Prerequisites)', '').trim();
                 lastDept.addCourse(new classes.Course(courseName));
                 lastCourse = getLast(lastDept.courses);
@@ -90,38 +90,31 @@ function parseForClasses(htmlBody) {
             } else if (row.is("tr[valign='top'][bgcolor='#FFFFCC']") || row.is("tr[valign='top']")) {
                 let sectionData = {};
                 let str = '';
-
                 row.find('td').each(function (i) {
                     let cell = $(this);
                     let cellText = cell.text();
                     switch (i) {
                         case 0:
                             sectionData.classCode = cellText;
-                            str += sectionData.classCode + "  ";
                             break;
                         case 1:
                             sectionData.classType = cellText;
-                            str += sectionData.classType + "  ";
                             break;
                         case 2:
                             sectionData.sectionCode = cellText;
-                            str += sectionData.sectionCode + "  ";
                             break;
                         case 3:
                             sectionData.units = cellText;
-                            str += sectionData.units + "  ";
                             break;
                         case 4:
                             sectionData.instructors = cell.html().split("<br>");
                             if (getLast(sectionData.instructors) === '') {
                                 sectionData.instructors.pop();
                             }
-                            str += sectionData.instructors + "  ";
                             break;
                         case 5:
                             sectionData.times = cell.html().split("<br>");
                             if (sectionData.times) {
-
                             }
                             sectionData.times.forEach(function (currentValue, index, array) {
                                 array[index] = array[index].replace('&#xA0;', '');
@@ -130,50 +123,40 @@ function parseForClasses(htmlBody) {
                                     array[index] = 'TBA'
                                 }
                             });
-                            str += sectionData.times + "  ";
                             break;
                         case 6:
                             cell.find('a').each(function (j, elem) {
                                 $(this).replaceWith($(this).text());
                             });
                             sectionData.places = cell.html().split("<br>");
-                            str += sectionData.places + "  ";
                             break;
                         case 7:
                             sectionData.finalExamDate = cellText.trim();
                             if (sectionData.finalExamDate.length === 0) {
                                 sectionData.finalExamDate = 'NONE';
                             }
-                            str += sectionData.finalExamDate + "  ";
                             break;
                         case 8:
                             sectionData.maxCapacity = cellText;
-                            str += sectionData.maxCapacity + "  ";
                             break;
                         case 9:
                             sectionData.numCurrentlyEnrolled = cellText;
-                            str += sectionData.numCurrentlyEnrolled + "  ";
                             break;
                         case 10:
                             sectionData.numOnWaitlist = cellText;
-                            str += sectionData.numOnWaitlist + "  ";
                             break;
                         case 11:
                             sectionData.numRequested = cellText;
-                            str += sectionData.numRequested + "  ";
                             break;
                         case 12:
                             sectionData.numNewOnlyReserved = cellText;
-                            str += sectionData.numNewOnlyReserved + "  ";
                             break;
                         case 13:
                             sectionData.restrictions = cellText.split('and');
                             //TODO: Find out what 'or' means with restrictions
-                            str += sectionData.restrictions + "  ";
                             break;
                         case 16:
                             sectionData.status = cellText;
-                            str += sectionData.status + "  ";
                             break;
                         default:
                             break;
@@ -183,14 +166,12 @@ function parseForClasses(htmlBody) {
                 lastDept = getLast(lastSchool.departments);
                 lastCourse = getLast(lastDept.courses);
                 lastCourse.sections.push(new classes.Section(sectionData));
-                // console.log(str);
-                if (row.next().prop("valign") === undefined && !row.next().hasClass("blue-bar")) {
+                if (!row.next().hasClass("blue-bar") && row.next().prop("valign") === undefined) {
                     getLast(lastCourse.sections).addComment(row.next().text());
                 }
             }
 
         });
-
     return schools;
 }
 
