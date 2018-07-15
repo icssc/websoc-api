86bee0c50a2304a1413e591b0910f1837db7e3c2
Nathan <nathan.abebayehu@gmail.com>
2018-07-15 04:06:55 -0700

    Primary data parsing refactored to use XML parsing instead of screen scraping.

--- websocapi.js
+++ websocapi.js
@@ -19,20 +19,17 @@ function callWebSocAPI({
         throw new Error("You must specify a building code if you specify a room number")
     }
 
-    let codedTerm = getCodedTerm(term.toLowerCase());
-    let codedDiv = getCodedDiv(division.toLowerCase());
-
     const postData = {
         url: "https://www.reg.uci.edu/perl/WebSoc",
         form: {
-            Submit: 'Display Web Results',
-            YearTerm: codedTerm,
+            Submit: 'Display XML Results',
+            YearTerm: getCodedTerm(term.toLowerCase()),
             ShowComments: 'on',
             ShowFinals: 'on',
             Breadth: GE,
             Dept: department,
             CourseNum: courseNum,
-            Division: codedDiv,
+            Division: getCodedDiv(division.toLowerCase()),
             CourseCodes: courseCodes,
             InstrName: instructorName,
             CourseTitle: courseTitle,
@@ -95,195 +92,68 @@ function getCodedDiv(div) {
 }
 
 function parse(htmlBody) {
-    const $ = cheerio.load(htmlBody);
-    const root = $('.course-list');
+    const $ = cheerio.load(htmlBody, {xmlMode: true, normalizeWhitespace: true});
+    const root = $('websoc_results > course_list');
 
     const schools = [];
 
-    let lastSchool;
-    let row;
-    let lastDept;
-    let lastCourse;
-    let numCols;
+    root.find('school').each(function () {
+        const school = $(this);
+        const schoolObj = new classes.School(school.attr("school_name"));
+        schools.push(schoolObj);
+        school.find('school_comment').each(function () {schoolObj.addComment($(this).text());});
 
-    root.find('table').find('tbody').find('tr').each(
-        function () {
-            row = $(this);
+        school.find('department').each(function () {
+            const dept = $(this);
+            const deptObj = new classes.Department([dept.attr('dept_code'), dept.attr('dept_name')]);
+            schoolObj.addDepartment(deptObj);
+            dept.find('department_comment').each(function () {deptObj.addComment($(this).text());});
+            dept.find('course_number_range_comment').each(function () {deptObj.addComment($(this).text());});
+            dept.find('course_code_range_comment').each(function () {deptObj.addComment($(this).text());});
 
-            if (row.hasClass("college-title")) {
-                let newSchool = new classes.School(row.children().text());
-                newSchool.addComment(row.next().hasClass("college-comment") ? sanitize(row.next().text()) : '');
+            dept.find('course').each(function () {
+                const course = $(this);
+                const courseObj = new classes.Course([course.attr('course_title'), course.attr('course_number')]);
+                deptObj.addCourse(courseObj);
+                course.find('course_comment').each(function () {courseObj.addComment($(this).text());})
 
-                lastSchool = newSchool;
-                schools.push(newSchool);
-            } else if (row.hasClass("dept-title")) {
-                let newDept = new classes.Department(row.text());
-                newDept.addComment(row.next().hasClass("dept-comment") ? sanitize(row.next().text()) : '');
+                course.find('section').each(function () {
+                    const section = $(this);
+                    const meetings = [];
+                    const sectionData = {
+                        classCode: $('course_code', section).text(),
+                        classType: $('sec_type', section).text(),
+                        sectionCode: $('sec_num', section).text(),
+                        units: $('sec_units', section).text(),
+                        instructors: $('sec_instructors > instructor', section).map(function () {return $(this).text()}).get(),
+                        finalExam: $('sec_final', section).text().trim(),
+                        maxCapacity: $('sec_enrollment > sec_max_enroll', section).text(),
+                        numCurrentlyEnrolled: $('sec_enrollment > sec_enrolled', section).text(),
+                        numOnWaitlist: $('sec_enrollment > sec_waitlist', section).text() === '' ? '0' : $('sec_enrollment > sec_waitlist', section).text(),
+                        numRequested: $('sec_enrollment > sec_enroll_requests', section).text(),
+                        restrictions: $('sec_restrictions', section).text(),
+                        status: $('sec_status', section).text(),
+                        comments: $('sec_comment', section).text()
+                    };
 
-                lastSchool.addDepartment(newDept);
-                lastDept = newDept;
-            } else if (row.hasClass("num-range-comment") || row.hasClass("ccode-range-comment")) {
-                if (lastDept.comments === '') {
-                    lastDept.addComment(sanitize(row.text()));
-                } else {
-                    lastDept.addComment('\n\n' + sanitize(row.text()));
-                }
-            } else if (row.is("tr[valign='top'][bgcolor='#fff0ff']")) {
-                let courseName = [sanitize(row.text()), row.find('b').text()];
-                let newCourse = new classes.Course(courseName);
-                lastDept.addCourse(newCourse);
-                newCourse.addComment(sanitize(row.next().find('.Comments').text()));
-                lastCourse = newCourse;
-            } else if (row.is("tr[bgcolor='#E7E7E7'][align='left']")) {
-                numCols = row.children().length;
-            } else if (row.is("tr[valign='top'][bgcolor='#FFFFCC']") || row.is("tr[valign='top']")) {
-                let newSection = new classes.Section(buildSectionData($, row, numCols));
-                lastCourse.sections.push(newSection);
+                    $('sec_meetings > sec_meet', section).each(function () {
+                        meetings.push([$('sec_days', this).text() + $('sec_time', this).text(), $('sec_bldg', this).text() + ' ' + $('sec_room', this).text()]);
+                    });
 
-                if (row.next().attr() !== undefined) {
-                    let attrs = Object.keys(row.next().attr());
-                    if (attrs.length === 0 || (attrs.length === 1 && row.next().attr('bgcolor') === '#FFFFCC')) {
-                        newSection.addComment(sanitize(row.next().text()));
-                    }
-                }
-            }
+                    sectionData.meetings = meetings;
+                    const possibleNewOnlyNum = $('sec_enrollment > sec_new_only_reserved', section).text();
+                    sectionData.numNewOnlyReserved = possibleNewOnlyNum === sectionData.classCode ? '0' : possibleNewOnlyNum;
+
+                    courseObj.addSection(new classes.Section(sectionData));
+                    console.log(sectionData);
+                });
+            });
+
+            // console.log(schoolObj);
         });
+    });
 
     return schools;
-}
-
-function buildSectionData(jquery, row, columnCount) {
-    let sectionData = {};
-
-    row.find('td').each(function (i) {
-        let cell = jquery(this);
-        let cellText = cell.text();
-
-        switch (i) {
-            case 0:
-                sectionData.classCode = cellText;
-                break;
-            case 1:
-                sectionData.classType = cellText;
-                break;
-            case 2:
-                sectionData.sectionCode = cellText;
-                break;
-            case 3:
-                sectionData.units = cellText;
-                break;
-            case 4:
-                sectionData.instructors = cell.html().split("<br>");
-                if (sectionData.instructors.slice(-1)[0] === '') {
-                    sectionData.instructors.pop();
-                }
-                break;
-            case 5:
-                sectionData.times = cell.html().split("<br>");
-                sectionData.times.forEach(function (currentValue, index, array) {
-                    if (currentValue.includes('TBA')) {
-                        array[index] = 'TBA';
-                    } else {
-                        array[index] = sanitize(currentValue);
-                    }
-                });
-                break;
-            case 6:
-                cell.find('a').each(function () {
-                    jquery(this).replaceWith(jquery(this).text());
-                });
-                sectionData.places = cell.html().split("<br>");
-                break;
-            case 7:
-                sectionData.finalExamDate = cellText.trim();
-                if (sectionData.finalExamDate.length === 0) {
-                    sectionData.finalExamDate = 'NONE';
-                }
-                break;
-            case 8:
-                sectionData.maxCapacity = cellText;
-                break;
-            case 9:
-                sectionData.numCurrentlyEnrolled = cellText;
-                break;
-            default:
-                break;
-        }
-
-        if (columnCount === 17) {
-            switch (i) {
-                case 10:
-                    sectionData.numOnWaitlist = cellText;
-                    break;
-                case 11:
-                    sectionData.numRequested = cellText;
-                    break;
-                case 12:
-                    sectionData.numNewOnlyReserved = cellText;
-                    break;
-                case 13:
-                    sectionData.restrictions = cellText;
-                    break;
-                case 16:
-                    sectionData.status = cellText;
-                    break;
-                default:
-                    break;
-            }
-        } else if (columnCount === 16) {
-            sectionData.numNewOnlyReserved = 0;
-            switch (i) {
-                case 10:
-                    sectionData.numOnWaitlist = cellText;
-                    break;
-                case 11:
-                    sectionData.numRequested = cellText;
-                    break;
-                case 12:
-                    sectionData.restrictions = cellText;
-                    break;
-                case 15:
-                    sectionData.status = cellText;
-                    break;
-                default:
-                    break;
-            }
-        } else if (columnCount === 15) {
-            sectionData.numOnWaitlist = 0;
-            sectionData.numNewOnlyReserved = 0;
-            switch (i) {
-                case 10:
-                    sectionData.numRequested = cellText;
-                    break;
-                case 11:
-                    sectionData.restrictions = cellText;
-                    break;
-                case 14:
-                    sectionData.status = cellText;
-                    break;
-                default:
-                    break;
-            }
-        }
-    });
-    return sectionData;
-}
-
-function sanitize(input) {
-    let sanitizedString = input;
-    if (typeof input === 'object') {
-        sanitizedString = input[0].replace(new RegExp(escapeRegExp(input[1]) + '|\\(Co-courses\\)|\\(Prerequisites\\)', 'g'), '');
-    } //TODO: Find a better solution for this
-    sanitizedString = sanitizedString.replace(/\t+|&#xA0|;/g, '');
-    sanitizedString = sanitizedString.replace(/[^\S\r\n]+/g, ' ');
-    sanitizedString = sanitizedString.replace(/- /, '-');
-    sanitizedString = sanitizedString.trim();
-    sanitizedString = sanitizedString.replace(/\n \n /g, '\n');
-    return sanitizedString;
-}
-
-function escapeRegExp(str) {
-    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 }
 
 module.exports.callWebSocAPI = callWebSocAPI;
